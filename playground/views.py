d4915a15782d5289ba102dd33884fe2492bcd6a9
Logan Jones <loganjones@node-1w7jra21oto59vkggwdp8u5wd.ipv6.telus.net>
2023-01-18 14:58:12 -0700

    Testing request handler

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
-# Path: playground/urls.py
+# Path: playground/urls.p
 def say_hello(request):
     return HttpResponse('Hello World!')
 
